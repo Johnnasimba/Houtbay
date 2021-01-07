7c3d75d4857db5a88541ce35aa6f02c777677433
JOHN NASIMBA <nasimba4john@gmail.com>
2021-01-07 21:35:36 +0200

    Updating layout of about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,11 +1,19 @@
 @extends('layouts.mainLayout')
 
 @section('body')
-<br><br>
-<h4 class="container center">This is page is designed to help Houtbay residents look for Jobs online there by avoid sitting besides the road everyday.
-    You can support this page simply by sharing it.
-</h4><br><br>
-<h6 class="container center">For more information contact John Nasimba using the contact information provided down below</h6>
-
+<div class="container" id="about-page">
+    <h4 class="container center">This is developed to help Hout bay residents look for Jobs online there by avoid sitting besides the road everyday.
+    You can support this page simply by sharing it.</h4>
+    <h6 class="container center">For more information contact John Nasimba using the contact information provided below</h6>
+</div>
+<style>
+    #about-page {
+        min-height: 100vh;
+    }
+    #about-page h4, h6 {
+        text-align: center;
+        margin-top: 100px;
+    }
+</style>
 
 @endsection
