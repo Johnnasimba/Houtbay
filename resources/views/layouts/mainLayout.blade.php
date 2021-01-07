1cfb059f8300fe7f5bc4db34c594d0b09d3d796b
JOHN NASIMBA <nasimba4john@gmail.com>
2021-01-08 00:17:01 +0200

    Updating css links for production purposes

--- resources/views/layouts/mainLayout.blade.php
+++ resources/views/layouts/mainLayout.blade.php
@@ -7,13 +7,13 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">     
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css ">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link href="{{URL::asset('/css/card.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{URL::asset('/css/categories.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{URL::asset('/css/header.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{URL::asset('/css/footer.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{URL::asset('/css/applicant.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{URL::asset('/css/admin.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{URL::asset('/css/addApplicant.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/card.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/categories.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/header.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/footer.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/applicant.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/admin.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/addApplicant.css') }}" rel="stylesheet" type="text/css">
  
     <title>Hout Bay Job Seekers</title>
   </head>
@@ -102,8 +102,8 @@
         </style>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/1936634b09.js" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="{{ URL::asset('js/script.js') }}"></script>
-    <script type="text/javascript" src="{{ URL::asset('js/applicant.js') }}"></script>
+    <script type="text/javascript" src="{{  asset('js/script.js') }}"></script>
+    <script type="text/javascript" src="{{  asset('js/applicant.js') }}"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     
